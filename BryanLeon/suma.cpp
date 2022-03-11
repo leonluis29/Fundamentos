488d24ce6d289147545ba0d68d9524b12196fbc8
leonluis29 <luis.leon.rodriguez@utelvt.edu.ec>
2022-03-11 12:58:55 -0500

    Creado desde mi computador

---
+++ BryanLeon/suma.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+	float A,B,C;
+	cout<<"Sumatoria"<<endl;
+	cout<<"Ingrese 2 numeros: ";
+	cin>>A>>B;
+	//Sumatoria
+	C=A+B;
+	cout<<"El resultado de:\n "<<A<<"+"<<B<<"="<<C;
+	return 0;
+}
