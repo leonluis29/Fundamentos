6eca7fe285cba5cbdbda69eee63b6a195761b3df
Stalin Francis <educaysoft@gmail.com>
2022-03-11 08:47:33 -0500

    Actualizaciones desde mi laptop hp

--- FrancisStalin/suma.cpp
+++ FrancisStalin/suma.cpp
@@ -5,8 +5,10 @@ using namespace std;
 int main()
 {
 	float A,B,C;
+	cout<<"Ingrese los valore de A B: ";
 	cin>>A>>B;
 	C=A+B;
-	cout<<C;
+	cout<<"El resultado es : ";
+	cout<<C<<endl;
 	return 0;
 }
