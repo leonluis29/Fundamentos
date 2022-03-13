3472e58e7e98ef4a0eb182273e993eb6ae8742ff
Girabel <andreina.penafiel.ramirez@utelvt.edu.ec>
2022-03-12 23:00:05 -0500

    creado desde mi lapto

---
+++ GirabelPeñafiel/suma.cpp
@@ -0,0 +1,12 @@
+
+
+#include<iostream>
+using namespace std;
+int main (){
+float A,B,C;
+cout<<"Ingrese 2 numeros: ";
+cin>>A>>B;
+C=A+B;
+cout<<"El resultado de:  "<<A<<"+"<<B<<"="<<C;
+return 0;
+}
