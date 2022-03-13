46ac928c7025c565ceeecc7b908d697e86111a5a
Joseluis198 <jose.proano.tenorio@utelvt.edu.ec>
2022-03-12 21:33:27 -0500

    creado desde mi celular

---
+++ Proañojoseluis/suma.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	float A,B,C;
+	cout<<"Ingrese los valores de A B:  ";
+	cin>>A>>B;
+	C=A+B;
+	cout<<"El resultado es  :";
+	cout<<C<<endl;
+	return 0;
+}
