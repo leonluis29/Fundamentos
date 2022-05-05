32a2ec67ebdc08ac93916fa968a4ab8c505e47e0
leonluis29 <luis.leon.rodriguez@utelvt.edu.ec>
2022-05-05 12:14:13 -0500

    Archivos nuevos en la carpeta ACTIVIDAD-B2

--- BryanLeon/ACTIVIDAD-B2/LeonBryan-laedad.cpp
+++ BryanLeon/ACTIVIDAD-B2/LeonBryan-laedad.cpp
@@ -1,3 +1,38 @@
 /*4. Diagrama de flujo y codigo en c++, de un programa que calcula la edad
  de una persona, el programa debe permitir ingresar la fecha actual, la fecha
  de nacimiento y mostrar cuantos años, meses y días tiene la persona*/
+#include<iostream>
+using namespace std;
+int main(){
+  int ee,ma,de,an,mn,dn,a,md,d,da,m;
+  cout<<"Ingrese su nombre: ";
+  cin>>ee;
+  cout<<"Ingrese su edad: ";
+  cin>>ma;
+  cout<<"Ingrese la fecha de nacimiento: ";
+  cin>>de;
+  cout<<"Ingrese la fecha actual: ";
+  cin>>an;
+  if(de>=dn)
+  {
+    d=de-dn;
+  }else
+  {
+    dn=dn+30;
+    ma=ma-1;
+    d=da-dn;
+  }
+  if(ma>=mn)
+  {
+    m=ma-mn;
+  }else
+  {
+    mn=mn+12;
+    ee=ee-1;
+    m=ma-mn;
+  }
+  cout<<"La persona se llama: "<<ee<<endl;
+  cout<<"Tiene "<<ma<<" años"<<endl;
+  cout<<"Tiene "<<de<<" dias"<<ma<<" años"<<endl;
+  return 0;
+}
